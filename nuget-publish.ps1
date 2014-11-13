a194704e7742472fa39afeee93bfd31c070294ba
Michael Logutov <logutov@gmail.com>
2014-11-13 20:21:25 +0300

    added missed files

---
+++ nuget-publish.ps1
@@ -0,0 +1,11 @@
+cls
+
+$id = ((Get-Item -Path ".\..").Name)
+iex "nuget pack -build -Prop Configuration=Release -sym $id\$id.csproj" -ErrorAction Stop
+
+$package_file = @(Get-ChildItem "*.nupkg" -Exclude "*.symbols.*" | Sort-Object -Property CreationTime -Descending)[0]
+$package_file.Name
+
+iex ("nuget push " + $package_file.Name)
+
+Remove-Item *.nupkg
