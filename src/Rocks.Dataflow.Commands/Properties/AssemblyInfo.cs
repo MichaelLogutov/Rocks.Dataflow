dcdb16dae8088488a1784c9d7aaf85b1ce4bd77e
Michael Logutov <logutov@gmail.com>
2014-11-13 14:39:51 +0300

    added commands

---
+++ src/Rocks.Dataflow.Commands/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("Rocks.Dataflow.Commands")]
+[assembly: AssemblyDescription ("A mix of command/query pattern implementation and Microsoft TPL Dataflow.")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Michael Logutov")]
+[assembly: AssemblyProduct ("Rocks.Dataflow.Commands")]
+[assembly: AssemblyCopyright ("Copyright © Michael Logutov 2014")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+[assembly: ComVisible (false)]
+[assembly: Guid ("c0c46c11-8149-4f7e-b5bb-81ba069333cf")]
+[assembly: AssemblyVersion ("1.0.*")]
+[assembly: InternalsVisibleTo ("Rocks.Dataflow.Commands.Tests")]
