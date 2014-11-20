ff80f83ffcf013d65ffd34946c20b7c4c2c500df
Michael Logutov <logutov@gmail.com>
2014-11-20 17:29:49 +0300

    exception logger

---
+++ src/Rocks.Dataflow.Tests/FluentTests/ExceptionHandlingTests/FluentSplitJoinAsyncTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rocks.Dataflow.Fluent;
+using Rocks.Dataflow.SplitJoin;
+
+namespace Rocks.Dataflow.Tests.FluentTests.ExceptionHandlingTests
+{
+	[TestClass]
+	public class FluentSplitJoinAsyncTests
+	{
+		[TestMethod]
+		public async Task SplitTo_WithDefaultExceptionLogger_OneItemsThrows_LogsTheException ()
+		{
+			// arrange
+			var exceptions = new ConcurrentBag<Exception> ();
+			var failed_items = new ConcurrentBag<object> ();
+
+			var sut = DataflowFluent
+				.ReceiveDataOfType<string> ()
+				.SplitToAsync<char> (async x =>
+				{
+					await Task.Yield ();
+
+					if (x == "b")
+						throw new TestException ();
+
+					return x.ToCharArray ();
+				})
+				.SplitJoin ()
+				.WithDefaultExceptionLogger ((exception, item) =>
+				{
+					exceptions.Add (exception);
+					failed_items.Add (item);
+				})
+				.CreateDataflow ();
+
+
+			// act
+			await sut.Process (new[] { "a", "b", "c" });
+
+
+			// assert
+			exceptions.Should ().HaveCount (1).And.ContainItemsAssignableTo<TestException> ();
+			failed_items.Should ().Equal ("b");
+		}
+
+
+		[TestMethod]
+		public async Task SplitProcess_WithDefaultExceptionLogger_OneItemsThrows_LogsTheException ()
+		{
+			// arrange
+			var exceptions = new ConcurrentBag<Exception> ();
+			var failed_items = new ConcurrentBag<object> ();
+
+			var sut = DataflowFluent
+				.ReceiveDataOfType<string> ()
+				.SplitToAsync<char> (async x =>
+				{
+					await Task.Yield ();
+					return x.ToCharArray ();
+				})
+				.SplitProcessAsync (async (s, x) =>
+				{
+					await Task.Yield ();
+
+					if (x == 'b')
+						throw new TestException ();
+				})
+				.SplitJoin ()
+				.WithDefaultExceptionLogger ((exception, item) =>
+				{
+					exceptions.Add (exception);
+					failed_items.Add (item);
+				})
+				.CreateDataflow ();
+
+
+			// act
+			await sut.Process (new[] { "a", "b", "c" });
+
+
+			// assert
+			exceptions.Should ().HaveCount (1).And.ContainItemsAssignableTo<TestException> ();
+			failed_items.ShouldAllBeEquivalentTo (new[] { new SplitJoinItem<string, char> ("b", 'b', 1) });
+		}
+	}
+}
